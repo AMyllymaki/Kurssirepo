5d10b99bfef8a22f74afebb12363439eed336eec
Anssi Myllymäki <anssi@pihlasoft.com>
2020-10-23 09:08:30 +0300

    Tehty ristille ja nollalle borderi

--- ristinolla/src/components/Ruutu.js
+++ ristinolla/src/components/Ruutu.js
@@ -12,15 +12,18 @@ function Ruutu(props) {
         if (props.symbol === "X") {
             return (
                 <svg width="100" height="100">
-                    <line x1="25" y1="25" x2="75" y2="75" stroke="blue" strokeWidth="10"  strokeLinecap="round"/>
+
+                    <line x1="25" y1="25" x2="75" y2="75" stroke="black" strokeWidth="15" strokeLinecap="round" />
+                    <line x1="75" y1="25" x2="25" y2="75" stroke="black" strokeWidth="15" strokeLinecap="round" />
+                    <line x1="25" y1="25" x2="75" y2="75" stroke="blue" strokeWidth="10" strokeLinecap="round" />
                     <line x1="75" y1="25" x2="25" y2="75" stroke="blue" strokeWidth="10" strokeLinecap="round" />
                 </svg>
             )
         }
         else if (props.symbol === "O") {
             return (<svg width="100" height="100">
-            
-                <circle cx="50%" cy="50%" r="35.35" fill="red" />
+
+                <circle cx="50%" cy="50%" r="35.35" stroke="black" strokeWidth="4" fill="red" />
             </svg>)
         }
 
@@ -31,7 +34,7 @@ function Ruutu(props) {
 
         < Grid item xs={4} >
             <div onClick={(e) => props.onClick(props.value)} style={{ backgroundColor: "white", width: 100, height: 100, borderWidth: 1, border: '2px solid black' }}>
-                
+
                 {haeSymboli()}
             </div>
         </Grid >
